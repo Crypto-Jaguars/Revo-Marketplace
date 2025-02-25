88512ed6f77519e74db4663c8f9a643a8b1c4b0c
arebisola <arebisolaolufisoye@gmail.com>
2025-02-25 01:18:59 +0100

    Testing

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@ const withNextIntl = require('next-intl/plugin')();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ... your existing config
+  images: {
+    domains: ['farm-images.example.com'], // Add your image domains
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  }
 }
 
 module.exports = withNextIntl(nextConfig); 
