bc05dde195c5d0dda0c799ff8c3e679e3ddf029d
Mario Fernando Araya Royo <arayaroyo686@gmail.com>
2025-02-28 16:39:17 -0600

    add HeroSection component to Home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,24 +1,28 @@
 'use client';
 
 import Bounded from '@/components/Bounded';
+import HeroSection from '@/components/herosection/heroSection';
 import { useTranslations } from 'next-intl';
 
 export default function Home() {
   const t = useTranslations('HomePage');
 
   return (
-    <Bounded center={true}>
-      <div className="flex flex-col justify-center items-center w-full h-full mt-0 md:mt-20 gap-4">
-        <h1 className="flex flex-col items-center">
-          <span className="text-[40px] md:text-[80px] font-bold text-primary_green">
-            {t('title')}
-          </span>
-          <span className="text-[40px] md:text-[80px] text-white">{t('subtitle')}</span>
-        </h1>
-        <p className="text-[16px] md:text-[20px] w-full md:max-w-[60%] text-white text-center">
-          {t('description')}
-        </p>
-      </div>
-    </Bounded>
+    <>
+      <HeroSection />
+      <Bounded center={true}>
+        <div className="flex flex-col justify-center items-center w-full h-full mt-0 md:mt-20 gap-4">
+          <h1 className="flex flex-col items-center">
+            <span className="text-[40px] md:text-[80px] font-bold text-primary_green">
+              {t('title')}
+            </span>
+            <span className="text-[40px] md:text-[80px] text-white">{t('subtitle')}</span>
+          </h1>
+          <p className="text-[16px] md:text-[20px] w-full md:max-w-[60%] text-white text-center">
+            {t('description')}
+          </p>
+        </div>
+      </Bounded>
+    </>
   );
 }
