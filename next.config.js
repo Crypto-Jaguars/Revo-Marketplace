7a3485d143ca5245bb35d4be6b15e221c3cfa751
Francisco Campos Díaz <sasasamaes@users.noreply.github.com>
2025-03-22 02:58:01 -0600

    sharp

--- next.config.js
+++ next.config.js
@@ -3,7 +3,18 @@ const withNextIntl = require('next-intl/plugin')();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['farm-images.example.com'], // Add your image domains
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'farm-images.example.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'openweathermap.org',
+        pathname: '/img/wn/**',
+      }
+    ],
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   }
