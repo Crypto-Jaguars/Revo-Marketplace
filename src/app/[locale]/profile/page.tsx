c0894d666617a5c33009bcc1c1cc25a9ad9a8205
Benjtalkshow <chinedubenj@gmail.com>
2025-08-04 15:49:26 +0100

    initialize profile page

---
+++ src/app/[locale]/profile/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <main className='w-full h-screen flex items-center justify-center bg-sky-400 font-2xl text-white'>
+      <h1 className='text-4xl text-white'>Profile Page</h1>
+    </main>
+  )
+}
+
+export default page
