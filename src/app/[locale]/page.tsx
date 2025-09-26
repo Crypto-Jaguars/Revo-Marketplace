0580f5676108c1f5125c0a078ad7fbd9071fb7c3
davedumto <ejeredavid2001@gmail.com>
2025-09-26 09:29:26 +0100

    feat: added a new waitlist cts component and then made some other minor fixes

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,6 +5,7 @@ import HeroSection from '@/components/herosection/heroSection';
 import { useTranslations } from 'next-intl';
 import { AboutUs } from '@/components/sections/about-us/AboutUs';
 import { HowWeWork } from '@/components/sections/how-we-work/HowWeWork';
+import { WaitlistCta } from '@/components/sections/waitlist-cta/WaitlistCta';
 import CtaSection from '@/components/cta/CtaSection';
 import HeroSectionNew from '@/components/herosection/herosectionnew';
 
@@ -17,6 +18,7 @@ export default function Home() {
       <HeroSectionNew />
       <AboutUs />
       <HowWeWork />
+      <WaitlistCta />
       <CtaSection />
     </main>
   );
